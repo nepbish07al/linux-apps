2bb57429f962fa0de789d7ab332ec0805220ede9
BISHAL POUDEL <68413032+nepbish07al@users.noreply.github.com>
2022-05-06 14:04:31 +0545

    Update linux-apps.sh

--- linux-apps.sh
+++ linux-apps.sh
@@ -16,3 +16,5 @@ sudo apt install snapd
 
 sudo apt install vlc
 sudo apt install vscode
+wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
+sudo dpkg -i google-chrome-stable_current_amd64.deb
