6b757cf7aeacae796598a1fe8cb04d5e39edc316
BISHAL POUDEL <68413032+nepbish07al@users.noreply.github.com>
2022-05-08 13:08:37 +0545

    Update linux-apps.sh

--- linux-apps.sh
+++ linux-apps.sh
@@ -5,7 +5,7 @@ sudo dpkg --add-architecture i386
 sudo apt update
 sudo apt install libxml2:i386 libcanberra-gtk-module:i386 gtk2-engines-murrine:i386 libatk-adaptor:i386
 
-wget -O ~/adobe.deb ftp://ftp.adobe.com/pub/adobe/reader/unix/9.x/9.5.5/enu/AdbeRdr9.5.5-1_i386linux_enu.deb
+wget -O ftp://ftp.adobe.com/pub/adobe/reader/unix/9.x/9.5.5/enu/AdbeRdr9.5.5-1_i386linux_enu.deb
 
 dpkg -i AdbeRdr9.5.5-1_i386linux_enu.deb
 
